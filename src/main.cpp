79c6f71f7f527f94239192676ef7d42c7044b6b3
aza547 <alexander-12345@hotmail.co.uk>
2025-07-13 23:16:01 +0100

    prototyping - video & preview!

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,42 @@
 #include <vector>
 #include <obs.h>
 #include <iostream>
+#include <future>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    if (uMsg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+
+void WindowThread(std::promise<HWND> hwndPromise) {
+    const wchar_t CLASS_NAME[] = L"MyWindowClass";
+    HINSTANCE hInstance = GetModuleHandle(nullptr);
+
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    RegisterClassW(&wc);
+
+    HWND hwnd = CreateWindowExW(
+        0, CLASS_NAME, L"My Window",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        nullptr, nullptr, hInstance, nullptr
+    );
+
+    hwndPromise.set_value(hwnd);  // Send hwnd back to main thread
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = {};
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+}
 
 void load_module(const char* module) {
     obs_module_t *ptr = NULL;
@@ -26,12 +62,9 @@ void load_module(const char* module) {
     }
 }
 
-void logsignalstart(void *param, calldata_t *data) {
-    blog(LOG_INFO, "GOT START SIGNAL!!!!!");
-}
-
-void logsignalstop(void *param, calldata_t *data) {
-    blog(LOG_INFO, "GOT STOP SIGNAL!!!!!");
+void draw_callback(void* data, uint32_t cx, uint32_t cy) {
+    // Render the OBS preview scene here
+    obs_render_main_texture();
 }
 
 void output_signal_handler(void *data, calldata_t *cd) {
@@ -39,12 +72,12 @@ void output_signal_handler(void *data, calldata_t *cd) {
     
     // Try to get common signal parameters
     obs_output_t *output = (obs_output_t*)calldata_ptr(cd, "output");
-    const char *signal_name = calldata_string(cd, "last_error");
+    const char *signal_name = calldata_string(cd, "signal");
     long long code = calldata_int(cd, "code");
     const char *error = calldata_string(cd, "error");
     const char *path = calldata_string(cd, "path");
     
-    std::cout << "last_error: " << (signal_name ? signal_name : "unknown") << std::endl;
+    std::cout << "signal: " << (signal_name ? signal_name : "unknown") << std::endl;
     std::cout << "Output: " << output << std::endl;
     std::cout << "Code: " << code << std::endl;
     
@@ -52,6 +85,52 @@ void output_signal_handler(void *data, calldata_t *cd) {
     if (path) std::cout << "Path: " << path << std::endl;
     
     std::cout << "===================\n" << std::endl;
+}
+
+static void listEncoders(obs_encoder_type type)
+{
+	constexpr uint32_t hide_flags = OBS_ENCODER_CAP_DEPRECATED | OBS_ENCODER_CAP_INTERNAL;
+
+	size_t idx = 0;
+	const char *encoder_type;
+
+	while (obs_enum_encoder_types(idx++, &encoder_type)) {
+		if (obs_get_encoder_caps(encoder_type) & hide_flags || obs_get_encoder_type(encoder_type) != type) {
+			continue;
+		}
+
+		blog(LOG_INFO, "\t- %s (%s)", encoder_type, obs_encoder_get_display_name(encoder_type));
+	}
+};
+
+static void listSourceTypes()
+{
+  size_t idx = 0;
+  const char *src = nullptr;
+
+  while (obs_enum_source_types(idx++, &src)) {
+      blog(LOG_INFO, "\t- %s", src);
+  }
+}
+
+static void listInputTypes()
+{
+  size_t idx = 0;
+  const char *src = nullptr;
+
+  while (obs_enum_input_types(idx++, &src)) {
+     blog(LOG_INFO, "\t- %s", src);
+  }
+}
+
+static void listOutputTypes()
+{
+  size_t idx = 0;
+  const char *src = nullptr;
+
+  while (obs_enum_output_types(idx++, &src)) {
+      blog(LOG_INFO, "\t- %s", src);
+  }
 }
 
 Napi::Value StartOBS(const Napi::CallbackInfo& info) {
@@ -64,7 +143,11 @@ Napi::Value StartOBS(const Napi::CallbackInfo& info) {
 
   load_module("D:/checkouts/warcraft-recorder-obs-engine/obs-plugins/64bit/obs-x264.dll");
   load_module("D:/checkouts/warcraft-recorder-obs-engine/obs-plugins/64bit/obs-ffmpeg.dll");
-  // load_module("D:/checkouts/cpp-httplib/plugins/64bit/win-capture.dll");
+  load_module("D:/checkouts/cpp-httplib/plugins/64bit/win-capture.dll");
+
+  // obs-ffmpeg-nvenc.c
+  // obs-qsv11
+  // AMD?
 
   bool init = obs_initialized();
 
@@ -74,9 +157,7 @@ Napi::Value StartOBS(const Napi::CallbackInfo& info) {
       std::cout << "OBS is initialized!" << std::endl;
     }
 
-    // std::cout << "AHK" << std::endl;
-
-     const char* version = obs_get_version_string();
+    const char* version = obs_get_version_string();
     std::cout << "OBS version is: " << version <<  std::endl;
 
     const char* n = "obs_x264";
@@ -142,8 +223,8 @@ Napi::Value StartOBS(const Napi::CallbackInfo& info) {
 
     std::cout << "Set output settings" << std::endl;
     obs_data_t *settings = obs_data_create();
-obs_data_set_string(settings, "url", "D:/checkouts/warcraft-recorder-obs-engine/recording.mp4");  // Use "url" not "path"
-obs_data_set_string(settings, "format_name", "mp4");
+    obs_data_set_string(settings, "url", "D:/checkouts/warcraft-recorder-obs-engine/recording.mp4");  // Use "url" not "path"
+    obs_data_set_string(settings, "format_name", "mp4");
     obs_output_update(output, settings);
     obs_data_release(settings);
 
@@ -163,13 +244,25 @@ obs_data_set_string(settings, "format_name", "mp4");
 
     std::cout << "Create aenc" << std::endl;
     obs_encoder_t *aenc = obs_audio_encoder_create("ffmpeg_aac", "simple_aac", NULL, 0, NULL);
+    std::cout << "Set audio encoder settings" << std::endl;
+    obs_data_t *aenc_settings = obs_data_create();
+    obs_data_set_int(aenc_settings, "bitrate", 128);
+    obs_encoder_update(aenc, aenc_settings);
+    obs_data_release(aenc_settings);
+
     obs_output_set_audio_encoder(output, aenc, 0);
     
     std::cout << "Create scene and src" << std::endl;
     obs_scene_t *scene = obs_scene_create("Scene");
   
     std::cout << "Create display capture source" << std::endl;
-    obs_source_t *source = obs_source_create("color_source", "Color", NULL, NULL);
+    // Create settings for monitor capture
+    obs_data_t *monitor_settings = obs_data_create();
+    obs_data_set_int(monitor_settings, "monitor", 0);  // Monitor 0
+    obs_data_set_bool(monitor_settings, "capture_cursor", true);
+
+    obs_source_t *source = obs_source_create("monitor_capture", "Monitor", monitor_settings, NULL);
+    obs_data_release(monitor_settings);
 
     if (source) {
       obs_scene_add(scene, source);
@@ -194,6 +287,38 @@ obs_data_set_string(settings, "format_name", "mp4");
     signal_handler_connect(sh, "start", output_signal_handler, NULL);
     signal_handler_connect(sh, "stop", output_signal_handler, NULL);
 
+    std::cout << "List  a encoders" << std::endl;
+    listEncoders(OBS_ENCODER_AUDIO);
+    std::cout << "List  v encoders" << std::endl;
+    listEncoders(OBS_ENCODER_VIDEO);
+
+    std::cout << "List  src types" << std::endl;
+    listSourceTypes();
+    std::cout << "List  input types" << std::endl;
+    listInputTypes();
+    std::cout << "List  output types" << std::endl;
+    listOutputTypes();
+
+    std::promise<HWND> hwndPromise;
+    std::future<HWND> hwndFuture = hwndPromise.get_future();
+
+    std::thread winThread(WindowThread, std::move(hwndPromise));
+    HWND hwnd = hwndFuture.get();  // blocks until hwnd is ready
+
+    gs_init_data gs_data = {};
+
+    gs_data.adapter = 0;
+    gs_data.cx = 1920;  // Window width
+    gs_data.cy = 1080;  // Window height
+    gs_data.format = GS_BGRA;
+    gs_data.zsformat = GS_ZS_NONE;
+    gs_data.num_backbuffers = 1;
+    gs_data.window.hwnd = hwnd;
+
+    obs_display_t* display = nullptr;
+    display = obs_display_create(&gs_data, 0x0);
+    obs_display_add_draw_callback(display, draw_callback, NULL);
+    
 
     std::cout << "Start rec" << std::endl;
     bool started = obs_output_start(output);
@@ -204,20 +329,14 @@ obs_data_set_string(settings, "format_name", "mp4");
         std::cerr << "Failed to start recording!" << std::endl;
     }
 
-    blog(LOG_WARNING, "is active? %d", obs_output_active(output));
-
-    std::this_thread::sleep_for(std::chrono::milliseconds(5500)); 
-
-    const char* a = obs_output_get_last_error(output);
-    blog(LOG_WARNING, "last err? %s", a);
-
-        blog(LOG_WARNING, "is active? %d", obs_output_active(output));
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000)); 
     obs_output_stop(output);
-        blog(LOG_WARNING, "is active? %d", obs_output_active(output));
-    std::this_thread::sleep_for(std::chrono::milliseconds(5500)); 
-        blog(LOG_WARNING, "is active? %d", obs_output_active(output));
+    std::this_thread::sleep_for(std::chrono::milliseconds(5000)); 
+
 
     std::cout << "END FN" << std::endl;
+
+     winThread.join();  // or detach() if you don’t care about waiting
   return info.Env().Undefined();
 }
 
