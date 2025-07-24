f3904c0c3d014ae0175ee08db703fc6a123e110c
aza547 <alexander-12345@hotmail.co.uk>
2025-07-24 21:24:32 +0100

    prototyping

--- dist.js
+++ dist.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const path = require('path');
 
 // Make the dist directory.
-const dist = path.resolve(__dirname, 'dist', 'bin', '64bit');
-fs.mkdirSync(dist, { recursive: true });
+const dist = path.resolve(__dirname, 'dist');
+fs.mkdirSync(dist);
 
 // Copy the compiled .node file.
 const addonSrc = path.resolve(__dirname, 'build', 'Release', 'warcraft-recorder-obs-engine.node');
@@ -12,10 +12,10 @@ fs.copyFileSync(addonSrc, addonDest);
 
 // Now copy the .dll files we need.
 const binDir = path.resolve(__dirname, 'bin', '64bit');
-const dlls = fs.readdirSync(binDir).filter(file => file.endsWith('.dll'));
 
-dlls.forEach((file) => {
+fs.readdirSync(binDir).filter(file => file.endsWith('.dll')).forEach((file) => {
   const srcPath = path.join(binDir, file);
   const destPath = path.join(dist, 'bin', '64bit', file);
+  fs.mkdirSync(dist);
   fs.copyFileSync(srcPath, destPath);
 });
