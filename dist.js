09a8c6256dc606b84937e4ac0cdda0ea54de9774
aza547 <alexander-12345@hotmail.co.uk>
2025-07-24 21:26:04 +0100

    prototyping

--- dist.js
+++ dist.js
@@ -2,8 +2,9 @@ const fs = require('fs');
 const path = require('path');
 
 // Make the dist directory.
-const dist = path.resolve(__dirname, 'dist');
-fs.mkdirSync(dist);
+const distRoot = path.resolve(__dirname, 'dist');
+const distBin = path.join(distRoot, 'bin', '64bit');
+fs.mkdirSync(distBin, { recursive: true });
 
 // Copy the compiled .node file.
 const addonSrc = path.resolve(__dirname, 'build', 'Release', 'warcraft-recorder-obs-engine.node');
@@ -15,7 +16,6 @@ const binDir = path.resolve(__dirname, 'bin', '64bit');
 
 fs.readdirSync(binDir).filter(file => file.endsWith('.dll')).forEach((file) => {
   const srcPath = path.join(binDir, file);
-  const destPath = path.join(dist, 'bin', '64bit', file);
-  fs.mkdirSync(dist);
+  const destPath = path.join(distBin, file);
   fs.copyFileSync(srcPath, destPath);
 });
