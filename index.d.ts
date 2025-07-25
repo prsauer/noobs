b16f33fa3e065831c55bd932665ad633287bbac2
aza547 <alexander-12345@hotmail.co.uk>
2025-07-25 16:01:25 +0100

    display

--- index.d.ts
+++ index.d.ts
@@ -30,8 +30,6 @@ interface Noobs {
 
   ObsShowPreview(hwnd: Buffer, x: number, y: number, width: number, height: number): void;
   ObsHidePreview(): void;
-  ObsResizePreview(width: number, height: number): void;
-  ObsMovePreview(x: number, y: number): void;
 }
 
 declare const noobs: Noobs;
